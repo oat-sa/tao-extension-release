a502d1692d40967283d1fd5621287e6dba51524e
Aleh Hutnikau <hutnikau@1pt.com>
2018-05-04 16:49:33 +0300

    fix windows home environment variable

--- src/config.js
+++ src/config.js
@@ -29,8 +29,7 @@ const path = require('path');
  * @param {String} [fileName = .tao-extension-release] - the config file
  * @returns {config} the config manager
  */
-module.exports = function configFactory(dir = process.env.HOME, fileName = '.tao-extension-release'){
-
+module.exports = function configFactory(dir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'], fileName = '.tao-extension-release'){
     const configFile = path.normalize(`${dir}/${fileName}`);
 
     /**
