7cb29963a5c895681fc876d6d0f1eb356026b859
Anton Tsymuk <atsymuk@gmail.com>
2019-01-22 12:00:56 +0300

    add tests for loadConfig method of release module

---
+++ tests/unit/release/loadConfig/test.js
@@ -0,0 +1,139 @@
+/**
+ *
+ * Unit test the loadConfig method of module src/release.js
+ *
+ * @copyright 2019 Open Assessment Technologies SA;
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ */
+
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const test = require('tape');
+
+const sandbox = sinon.sandbox.create();
+
+//load the tested module and mock dependencies
+const config = {
+    load: () => ({}),
+    write: () => { },
+};
+const inquirer = {
+    prompt: () => ({}),
+};
+const opn = sandbox.spy();
+const release = proxyquire.noCallThru().load('../../../../src/release.js', {
+    './config.js': () => config,
+    inquirer,
+    opn: opn,
+})();
+
+test('should define loadConfig method on release instance', (t) => {
+    t.plan(1);
+
+    t.ok(typeof release.loadConfig === 'function', 'The release instance has loadConfig method');
+
+    t.end();
+});
+
+test('should load config', async (t) => {
+    t.plan(1);
+
+    const data = { token: 'testToken' };
+
+    sandbox.stub(config, 'load').returns(data);
+
+    await release.loadConfig();
+
+    t.ok(config.load.callCount === 1, 'Config has been loaded');
+
+    sandbox.restore();
+    t.end();
+});
+
+test('should open github token settings if there is no token in the config', async (t) => {
+    t.plan(2);
+
+    const clock = sandbox.useFakeTimers();
+
+    await release.loadConfig();
+
+    clock.tick(2000);
+
+    t.ok(opn.callCount === 1, 'Config has been loaded');
+    t.ok(opn.calledWith('https://github.com/settings/tokens'));
+
+    clock.restore();
+    sandbox.restore();
+    t.end();
+});
+
+test('should prompt user to provide a token if there is no token in the config', async (t) => {
+    t.plan(4);
+
+    sandbox.stub(inquirer, 'prompt').callsFake(({ type, name, message }) => {
+        t.ok(type === 'input', 'The type should be "input"');
+        t.ok(name === 'token', 'The param name should be token');
+        t.ok(message === 'I need a Github token, with "repo" rights (check your browser)  : ', 'Should disaplay appropriate message');
+
+        return {};
+    });
+
+    await release.loadConfig();
+
+    t.ok(inquirer.prompt.callCount === 1, 'Prompt has been initialised');
+
+    sandbox.restore();
+    t.end();
+});
+
+test('should validate provided token', async (t) => {
+    t.plan(2);
+
+    const validToken = 'hsajdf234jhsaj234dfhh234asj32dfh';
+    const invalidToken = 'invalidToken';
+
+    sandbox.stub(inquirer, 'prompt').callsFake(({ validate }) => {
+        t.ok(validate(validToken), 'Validate valid token');
+        t.notOk(validate(invalidToken), 'Validate invalid token');
+
+        return {};
+    });
+
+    await release.loadConfig();
+
+    sandbox.restore();
+    t.end();
+});
+
+test('should trim token', async (t) => {
+    t.plan(1);
+
+    sandbox.stub(inquirer, 'prompt').callsFake(({ filter }) => {
+        t.ok(filter('   testToken   ') === 'testToken', 'Validate valid token');
+
+        return {};
+    });
+
+    await release.loadConfig();
+
+    sandbox.restore();
+    t.end();
+});
+
+test('should save provided token', async (t) => {
+    t.plan(2);
+
+    const token = 'testToken';
+
+    sandbox.stub(config, 'write');
+
+    sandbox.stub(inquirer, 'prompt').returns({ token });
+
+    await release.loadConfig();
+
+    t.ok(config.write.callCount === 1, 'The config has been saved');
+    t.ok(config.write.calledWith({ token }), 'The token has been saved in the config');
+
+    sandbox.restore();
+    t.end();
+});
